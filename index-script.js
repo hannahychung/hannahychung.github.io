adbf138fc190a8682a5ed5ee48900962d2ce427b
Hannah Chung <hannah.y.chung@my.wheaton.edu>
2025-07-25 12:13:27 -0500

    home page + nav basic complete

---
+++ index-script.js
@@ -0,0 +1,24 @@
+const themeButton = document.getElementById("theme");
+const body = document.body;
+
+
+//functionality to change the visual theme
+
+themeButton.addEventListener('click', () => {
+    body.classList.toggle('inverted');
+});
+
+
+//to toggle the menu 
+
+const menuBtn = document.getElementById('menu-button');
+const closeMenuBtn = document.getElementById('close-menu');
+const fullscreenMenu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    fullscreenMenu.classList.add('active');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+    fullscreenMenu.classList.remove('active');
+});
